c27896637f6aded1fe34fe24f08a04e202470781
Oliver Zhang <bestozhang@gmail.com>
2017-01-21 13:05:10 -0800

    Added preliminary script to move ball up and down.

---
+++ Assets/Scripts/CircleMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleMovement : MonoBehaviour {
+
+	private float frequency = 5;
+	private float amplitude = 5;
+	private bool isGoingUp;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y > amplitude) {
+			isGoingUp = false;
+		} else if (transform.position.y < -1*amplitude) {
+			isGoingUp = true;
+		}
+		
+		if (isGoingUp) {
+			transform.position += Vector3.up * amplitude * frequency * Time.deltaTime;
+		} else {
+			transform.position += Vector3.down * amplitude * frequency  * Time.deltaTime;
+		}
+	}
+}
