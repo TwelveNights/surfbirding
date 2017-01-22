94d41c2e2ebb7fec2daade00dc548b87651a214b
Oliver Zhang <bestozhang@gmail.com>
2017-01-21 23:17:05 -0800

    Added enlarging behaviour.

--- Assets/Scripts/ShotBehaviour.cs
+++ Assets/Scripts/ShotBehaviour.cs
@@ -4,23 +4,11 @@ using UnityEngine;
 
 public class ShotBehaviour : MonoBehaviour {
 
-	[Range (0.1f, 1.0f)]
-	public float fadeSpeed = 1f;    // How fast alpha value decreases.
-	public Color fadeColor = new Color (0, 0, 0, 0);
-
-	private Material material;      // Used to store material reference.
-	private Color color;            // Used to store color reference.
+	private float scale = 0.0f;
 
 	// Use this for initialization
 	void Start () {
-
-		// Get reference to object's material.
-		material = GetComponent <Renderer> ().material;
-
-		// Get material's starting color value.
-		color = material.color;
-
-		AlphaFade ();
+		StartCoroutine(Enlarge());
 	}
 	
 	// Update is called once per frame
@@ -29,19 +17,13 @@ public class ShotBehaviour : MonoBehaviour {
 	}
 
 	// This method fades only the alpha.
-	IEnumerator AlphaFade () {
-		// Alpha start value.
-		float alpha = 1.0f;
+	IEnumerator Enlarge () {
 
 		// Loop until aplha is below zero (completely invisalbe)
-		while (alpha > 0.0f) {
+		while (scale < 1.0f) {
 			// Reduce alpha by fadeSpeed amount.
-			alpha -= fadeSpeed * Time.deltaTime;
-
-			// Create a new color using original color RGB values combined
-			// with new alpha value. We have to do this because we can't 
-			// change the alpha value of the original color directly.
-			material.color = new Color (color.r, color.g, color.b, alpha);
+			transform.localScale += new Vector3(1f, 1f, 0) * Time.deltaTime;
+			scale += 0.01f;
 
 			yield return null;
 		}
