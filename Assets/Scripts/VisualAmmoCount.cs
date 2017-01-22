466d2562edbffe8ecd93ea3706af40cb56bf5fb4
Jerry Chen <Jerry Chen>
2017-01-21 21:42:27 -0800

    I added some things to visualammocounter

--- Assets/Scripts/VisualAmmoCount.cs
+++ Assets/Scripts/VisualAmmoCount.cs
@@ -8,13 +8,23 @@ public class VisualAmmoCount : MonoBehaviour {
 
     public int ammo;
 
+    public int reloading = 0;
+
+    public RectTransform ammo1;
+    public RectTransform ammo2;
+
+
 	// Use this for initialization
 	void Start () {
+        ammo1 = GetComponent<RectTransform> ();
         ammo = ammoCount.ammunition;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetMouseButton(0) && reloading == 0)
+        {
+            reloading++;
+        }
 	}
 }
