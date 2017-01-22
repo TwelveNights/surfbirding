b9d9f61bac39feead18fa25d0c99b1ef5f622fa9
Oliver Zhang <bestozhang@gmail.com>
2017-01-21 19:45:29 -0800

    Fixed sinusoidal movement to meet specifications.

--- Assets/Scripts/CircleMovement.cs
+++ Assets/Scripts/CircleMovement.cs
@@ -4,90 +4,29 @@ using UnityEngine;
 
 public class CircleMovement : MonoBehaviour {
 
-	private static int MAX_AMPLITUDE = 5;
-	private static int MIN_AMPLITUDE = 1;
-	private static int MAX_FREQUENCY = 5;
-	private static int MIN_FREQUENCY = 1;
+	// Constants defining Amplitude and Frequency
+	private static float AMPLITUDE_STEP = 0.001f;
+	private static float START_AMPLITUDE = 3;
+	private static float FREQUENCY_STEP = 0.01f;
+	private static float START_FREQUENCY = 6;
 
-	private int trueFreq;
-	private int trueAmpl;
-
-	private int interFreq;
-	private int interAmpl;
-
-	private bool isGoingUp;
+	private float currentAmplitude;
+	private float currentFrequency;
 
 	// Use this for initialization
 	void Start () {
-		trueFreq = 1;
-		trueAmpl = 1;
-
-		interFreq = 1;
-		interAmpl = 1;
-
-		isGoingUp = true;
+		currentAmplitude = START_AMPLITUDE;
+		currentFrequency = START_FREQUENCY;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		Vector3 direction = (isGoingUp) ? Vector3.up : Vector3.down;
+		float currMousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition).y;
+		float increment = Mathf.Sin (Time.time) * currentAmplitude + currMousePos;
+		transform.position = Vector3.up * increment;
 
-		if (Mathf.Abs (transform.position.y) > trueAmpl) {
-			transform.position = direction * trueAmpl;
-			isGoingUp = !isGoingUp;
-			return;
-		}
-
-		if (Mathf.Abs (transform.position.y) < MIN_AMPLITUDE) {
-			trueAmpl = interAmpl;
-			trueFreq = interFreq;
-		}
-
-		if (Input.GetKeyDown (KeyCode.UpArrow) && interAmpl < MAX_AMPLITUDE) {
-			interAmpl += 1;
-		} else if (Input.GetKeyDown (KeyCode.DownArrow) && interAmpl > MIN_AMPLITUDE) {
-			interAmpl -= 1;
-		} else if (Input.GetKeyDown (KeyCode.RightArrow) && interFreq < MAX_FREQUENCY) {
-			interFreq += 1;
-		} else if (Input.GetKeyDown (KeyCode.LeftArrow) && interFreq > MIN_FREQUENCY) {
-			interFreq -= 1;
-		}
-
-		transform.position += direction * trueAmpl * trueFreq * Time.deltaTime;
+		currentAmplitude += AMPLITUDE_STEP;
+		currentFrequency += FREQUENCY_STEP;
 	}
-//		float mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
-//		float sinWave = Mathf.Sin (Time.time * trueFreq) * trueAmpl;
-//
-//		transform.position = new Vector3 (transform.position.x, sinWave - mousePos, transform.position.z);
 
-
-//		transform.position = transform.up * sinWave;
-
-//		if (Input.GetKey (KeyCode.UpArrow) && interAmpl < MAX_AMPLITUDE) {
-//			interAmpl += 0.25f;
-//		} else if (Input.GetKey (KeyCode.DownArrow) && interAmpl > MIN_AMPLITUDE) {
-//			interAmpl -= 0.25f;
-//		} else if (Input.GetKey (KeyCode.RightArrow) && interFreq < MAX_FREQUENCY) {
-//			interFreq += 0.25f;
-//		} else if (Input.GetKey (KeyCode.LeftArrow) && interFreq > MIN_FREQUENCY) {
-//			interFreq -= 0.25f;
-//		}
-//
-//		if ((isGoingUp && transform.position.y > 0.1f) ||
-//			(!isGoingUp && transform.position.y < 0.1f)) {
-//			trueFreq = interFreq;
-//			trueAmpl = interAmpl;
-//		}
-//
-//		if (Mathf.Abs (transform.position.y) > trueAmpl) {
-//			isGoingUp = !isGoingUp;
-//			trueFreq = interFreq;
-//			trueAmpl = interAmpl;
-//		}
-//
-//		Vector3 direction = (isGoingUp) ? Vector3.up : Vector3.down;
-//		transform.position = direction * Mathf.Sin (elapsedTime * trueFreq) * trueAmpl;
-//
-//		elapsedTime += Time.deltaTime;
-//
 }
