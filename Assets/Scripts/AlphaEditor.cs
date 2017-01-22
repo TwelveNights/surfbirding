3d940cb57c392bd182200629805aef9e623ab369
Jessica Hoang <jessica.hoang@alumni.ubc.ca>
2017-01-22 14:24:21 -0800

    Adding Transparent box behind credits, adding the Transparency script named AlphaEditor.cs

---
+++ Assets/Scripts/AlphaEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class AlphaEditor : MonoBehaviour {
+    
+    Image image;
+    public float alphaValue;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+
+        Color c = image.color;
+        c.a = alphaValue;
+        image.color = c;
+    }
+
+}
